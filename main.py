646cb0f4449ab8aea0c702a77eb55ce759f64a2a
nnw-2 <161835895+nnw-2@users.noreply.github.com>
2024-12-22 12:03:13 +0000

    Adding some Comments

---
+++ main.py
@@ -0,0 +1,2 @@
+#Only intended to work by using the music lab settings Length : 10 bars , Beats per bar : 7 , Beats split into : 4 , Range : 3 octave.
+#How many boxes to work with - height : 23    width : 280
