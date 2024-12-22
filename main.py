9b463d0e23f031190d61b0f79dfcda55e7742640
nnw-2 <161835895+nnw-2@users.noreply.github.com>
2024-12-22 12:46:13 +0000

    pynput import

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #Only intended to work by using the music lab settings Length : 10 bars , Beats per bar : 7 , Beats split into : 4 , Range : 3 octave.
 #How many boxes to work with - height : 23    width : 280
+from pynput.keyboard import Key , Controller
+import sys
+keyboard = Controller()
+
+constant = input("e i pi or ratio?")
+
+#start keyboard input unpon s key being pressed
+while 1:
+    if keyboard.pressed("s"):
+        break
+
+#checks for s key press to end program
+while 1:
+    if keyboard.pressed("s"):
+        sys.exit()
+    
